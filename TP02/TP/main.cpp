0c31f8b2876648d72bbc3312644c8bcb76e4db74
Luís Felipe Ramos Ferreira <luisfelipe_ramos@outlook.com>
2022-06-13 00:48:26 -0300

    uso de pqmelhorado

--- TP02/TP/main.cpp
+++ TP02/TP/main.cpp
@@ -1,47 +1,68 @@
 #include <iostream>
 #include <bits/stdc++.h>
 
-# define INF 0x3f3f3f3f
+/* Essa estrutura é um utilitário auxiliar para a criação de uma função de comparação entre pares de inteiros,
+utilizada como função de comparação da estrutura da fila de prioridades(priority queue) da biblioteca padrão. */
+struct Compare{
 
-void print(std::vector<std::vector<int>> graph, int rows, int cols){
-    for (int i = 0; i < rows; i++){
-        for (int j = 0; j < cols; j++){
-            std::cout << graph.at(i).at(j) << " ";
-        }
-        std::cout << std::endl;
+    public:
+
+    /* Descrição: função de comparação entre dois pares de inteiros, que compara o segundo valor de ambos os pares.
+    Entrada: dois pares de inteiros.
+    Saída: verdadeiro se o valor do primeiro par for maior, falso caso contrário. */
+    bool operator()(std::pair<int, int>& p1, std::pair<int, int>& p2){
+        return p1.second < p2.second;
     }
-}
+};
 
-typedef std::pair<int, int> iPair;
 
-int widestPath(std::list<std::pair<int, int>> graph[], int s, int t, int numberOfNodes){
+/* Descrição: Função para o cálculo do caminho de gargalo máximo entre dois vértices em um grafo
+Entrada:
+Saída: */
+int widestPathAlgorithm(std::vector<std::list<std::pair<int, int>>>& graph, int s, int t){
     s = s - 1;
     t = t - 1;
+
+    int numberOfNodes = graph.size();
 
     std::vector<int> widest(numberOfNodes, INT_MIN);
     widest.at(s) = INT_MAX;
 
-    std::priority_queue<int, std::vector<int>, std::greater<int>> pq;
-    pq.push(s);
+    std::vector<bool> visited(numberOfNodes, 0);
+
+    std::priority_queue<std::pair<int, int>, std::vector<std::pair<int, int>>, Compare> pq;
+    pq.push({s, INT_MAX});
 
     while (!pq.empty()){
-        int currSrcVertex = pq.top();
+        int currVertex = pq.top().first;
+        int currWidestValue = pq.top().second;
         pq.pop();
 
-        for (auto vertexWeightPair : graph[currSrcVertex]){
-            int currDestVertex = vertexWeightPair.first;
-            int currWeight = vertexWeightPair.second;
-            int distance = std::max(widest[currDestVertex], std::min(widest[currSrcVertex], currWeight));
+        if (visited.at(currVertex)) continue;
+        
+        visited.at(currVertex) = 1;
+        widest.at(currVertex) = currWidestValue;
+        
 
-            if (distance > widest[currDestVertex]){
-                widest[currDestVertex] = distance;
-                pq.push(currDestVertex);
-            }
+        for (auto vertexWeightPair : graph[currVertex]){
+            
+            int currNewVertex = vertexWeightPair.first;
+
+            if (visited.at(currNewVertex)) continue;
+
+            int currNewWeight = vertexWeightPair.second;
+            
+            int widestValue = std::min(widest.at(currVertex), currNewWeight);
+
+            pq.push({currNewVertex, widestValue});
+            
         }
+
     }
 
     return widest[t];
 }
+
 
 int main(int argc, char* argv[]){
 
@@ -49,20 +70,22 @@ int main(int argc, char* argv[]){
     int numberOfHighways;
     int numberOfConsults;
 
-    std::cin >> numberOfCities >>numberOfHighways >> numberOfConsults;
-
-    std::list<std::pair<int, int>> graph[numberOfHighways];
+    std::cin >> numberOfCities >> numberOfHighways >> numberOfConsults;
+   
+    std::vector<std::list<std::pair<int, int>>> graph(numberOfCities);
 
     int uCity, vCity, weight;
+  
     for (int i = 0; i < numberOfHighways; i++){
         std::cin >> uCity >> vCity >> weight;
         graph[uCity - 1].push_back({vCity - 1, weight});
     }
 
-    int s, t;
+    int src, dest;
+
     for (int i = 0; i < numberOfConsults; i++){
-        std::cin >> s >> t;
-        std::cout << widestPath(graph, s, t, numberOfHighways) << std::endl;
+        std::cin >> src >> dest;
+        std::cout << widestPathAlgorithm(graph, src, dest) << std::endl; 
     }
 
 
