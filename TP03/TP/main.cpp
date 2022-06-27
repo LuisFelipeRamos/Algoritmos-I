9a04a22d42bc324d687554f4eb4f228b909c944f
Luís Felipe Ramos Ferreira <luisfelipe_ramos@outlook.com>
2022-06-27 19:20:51 -0300

    TP finalizado

--- TP03/TP/main.cpp
+++ TP03/TP/main.cpp
@@ -2,8 +2,10 @@
 
 #include <bits/stdc++.h>
 
-
-bool tableFitsHistogram(std::vector<int>& histogram, int tableLength, int tableWidth){
+/* Descrição: essa função checa se uma determinada mesa, no caso, um retângulo, cabe dentro de um histograma.
+Entrada: referência para um vetor de inteiros que representa o histograma, assim como as dimensões da mesa/retângulo.
+Saída: verdadeiro se cabe na casa, falso caso contrário. */
+bool tableFitsHistogram(std::vector<int>& histogram, int& tableLength, int& tableWidth){
 
     int numberOfBars = int(histogram.size());
 
@@ -48,7 +50,10 @@ bool tableFitsHistogram(std::vector<int>& histogram, int tableLength, int tableW
     return false;
 }
 
-bool tableFitsHouse(std::vector<std::vector<int>>& houseMatrixHistograms, int tableLength, int tableWidth){
+/* Descrição: essa função chama, para cada histograma obtido no desing da casa, a função que checa se a mesa cabe no histograma.
+Entrada: vetor de histogramas para cada iteração de histogramas pelo design da casa, assim como as dimensões da mesa.
+Saída: verdadeiro se a mesa cabe na casa, falso caso contrário. */
+bool tableFitsHouse(std::vector<std::vector<int>>& houseMatrixHistograms, int& tableLength, int& tableWidth){
     for (std::vector<int> histogram : houseMatrixHistograms){
         if (tableFitsHistogram(histogram, tableLength, tableWidth)){
             return true;
@@ -59,6 +64,7 @@ bool tableFitsHouse(std::vector<std::vector<int>>& houseMatrixHistograms, int ta
 
 int main(int argc, char* argv[]){
 
+    /* Leitura das dimensões da casa. */
     int houseLength, houseWidth;
     std::cin >> houseLength >> houseWidth;
 
@@ -84,41 +90,47 @@ int main(int argc, char* argv[]){
             }
 
             if (!emptySpace){
-               
                 houseMatrixHistograms.at(i).at(j) = 0;
             }
             else{
-              
                 houseMatrixHistograms.at(i).at(j) = houseMatrixHistograms.at(i - 1).at(j) + 1;
             }
         }
     }
 
+    /* Leitura do número de mesas na lista de entrada. */
     int numberOfTables;
     std::cin >> numberOfTables;
 
+    /* Variáveis auxiliares para as dimensões e área da mesa. */
     int tableLength, tableWidth;
     int tableArea;
 
+    /* Variáveis que armazenam temporariamente a maior área e as dimensões da mesa que a possui. */
     std::pair<int, int> maxAreaTableSizes = {INT_MIN, INT_MIN};
     int maxArea = INT_MIN;
 
+    /* Maior dimensão da casa, útil para armazenar na matriz de mesas que cabem na casa quais cabem e quais não. */
     int maxHouseSide = houseLength > houseWidth ? houseLength : houseWidth;
-
     std::vector<std::vector<int>> tablesThatFitHouse(maxHouseSide + 1, std::vector<int>(maxHouseSide + 1, 2));
 
+    /* Laço percorrido para cada mesa na lista de entrada. */
     for (int i = 0; i < numberOfTables; i++){
 
+        /* Leitura das coordenadas da mesa. */
         std::cin >> tableLength >> tableWidth;
 
-        int tableFits = tablesThatFitHouse.at(tableLength).at(tableLength);
+        /* Variáveis para armazenar se a mesa rotacionada ou não cabe na casa. */
+        int tableFits = tablesThatFitHouse.at(tableLength).at(tableWidth);
         int tableFitsRotated = tablesThatFitHouse.at(tableWidth).at(tableLength);
 
+        /* Se não couber, é ignorada. */
         if ((tableFits == 0) || (tableFitsRotated == 0)){
             continue;
         }
 
-        if ((tableFits == 1) || (tableFitsRotated == 1)){
+        /* Se couber e estiver armazenada, sua área é analisada e a área máxima atualizada se necessário. */
+        else if ((tableFits == 1) || (tableFitsRotated == 1)){
             tableArea = tableLength * tableWidth;
             if (tableArea > maxArea){
                 maxArea = tableArea;
@@ -134,6 +146,8 @@ int main(int argc, char* argv[]){
             }
         }
 
+        /* Se não se sabe nada sobre as coordenadas, checa-se se ela cabe na casa, e, caso positivo, a área é analisada e a área máxima atualizada 
+        se necessário. */
         else if (tableFitsHouse(houseMatrixHistograms, tableLength, tableWidth)){
             for (int m = 0; m < tableLength + 1; m++){
                 for (int n = 0;  n < tableWidth + 1; n++){
@@ -157,6 +171,7 @@ int main(int argc, char* argv[]){
             }
         }
 
+        /* Se não couber, atualizamos na matriz auxiliar os tamanhos que não cabem na mesa junto com ela. */
         else{
             for (int m = tableLength; m < maxHouseSide + 1; m++){
                 for (int n = tableWidth; n < maxHouseSide + 1; n++){
@@ -165,13 +180,6 @@ int main(int argc, char* argv[]){
                 }
             }
         }        
-    }
-
-    for (int i = 0; i < tablesThatFitHouse.size(); i++){
-        for (int j = 0; j < tablesThatFitHouse.at(i).size(); j++){
-            std::cout << tablesThatFitHouse.at(i).at(j) << " ";
-        }
-        std::cout << "\n";
     }
 
     std::cout << maxAreaTableSizes.first << " " << maxAreaTableSizes.second << std::endl;
